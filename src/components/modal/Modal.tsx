4fb2d68c774d811f4fe0f25b99f6f970a8f0aa46
YourContender <dream-boy123@ukr.net>
2024-04-25 18:56:08 +0300

    add translate for modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import "./Modal.scss";
+import { t } from 'i18next';
 
 interface TestProps {
     showModal: boolean;
@@ -16,26 +17,26 @@ const Modal: FC<TestProps> = ({ showModal, setShowModal, priceModal }) => {
                     <div className="modal_title">
                         <button className="modal_title-btn" onClick={() => setShowModal(false)}>&times;</button>
                         <span className="modal_title-h1">GOD OF WAR RAGNAROK</span>
-                        <p className="modal_title-p">STANDARD EDITION</p>
+                        <p className="modal_title-p">{t("modal.title")}</p>
                     </div>
 
                     <div className="modal_card">
-                        <label>Credit card: </label>
+                        <label>{t("modal.card")}: </label>
                         <input type="text" placeholder="0000 0000 0000 0000" />
                     </div>
                     <div className="modal_expire">
                         <div>
-                            <label>Expire: </label>
+                            <label>{t("modal.expire")}: </label>
                             <input type="text" placeholder="MM/YY" />
                         </div>
 
                         <div>
-                            <label>CCV: </label>
+                            <label>{t("modal.cvv")}: </label>
                             <input type="text" placeholder="***"/>
                         </div>
                     </div>
                     <div className="modal_instant">
-                        <label>Instant: </label>
+                        <label>{t("modal.instant")}: </label>
                         <input type="text" placeholder="John Smith" />
                     </div>
 
@@ -43,27 +44,29 @@ const Modal: FC<TestProps> = ({ showModal, setShowModal, priceModal }) => {
                         <label>
                             <input type="checkbox" className='modal_click-checkbox'/>
                             <span className='modal_click-fake'></span>
-                            <span className='modal_click-text'>I agree with the <span className='modal_click-rules'>rules</span> of the site</span>
+                            <span className='modal_click-text'>
+                                {t("modal.text.part1")} 
+                                <span className='modal_click-rules'>
+                                    {t("modal.text.part2")}
+                                </span> 
+                                {t("modal.text.part3")}
+                            </span>
                         </label>
                     </div>
 
                     <div className="modal_instruction">
                         <span>
-                            Download of this product is subject to the PlayStation Network Terms 
-                            of Service and our Software Usage Terms plus any specific additional 
-                            conditions applying to this product. If you do not wish to accept 
-                            these terms, do not download this product. See Terms of Service for 
-                            more important information.
+                            {t("modal.descr")}
                         </span>
                     </div>
 
                     <div className="modal_sum">
-                        <span>YOUR TOTAL SUMMA:</span>
+                        <span>{t("modal.sum")}: </span>
                         <span className="modal_sum-num">{priceModal}</span>
                     </div>
 
                     <div className="modal_btn">
-                        <button>buy now</button>
+                        <button>{t("modal.btn")}</button>
                     </div>
                 </div>
             </div>
